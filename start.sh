ffb33991661db73362f83d27d1633f1c312197b7
Perry Mitchell <perry@perrymitchell.net>
2024-08-06 15:22:33 +0300

    Remove log

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ else
     echo "Config passed as environment variable"
     mkdir -p /etc/otel
     echo "$OTEL_CONFIG_BASE64" | base64 -d > /etc/otel/config.yaml
-    cat /etc/otel/config.yaml
 fi
 
 exec "$@"
